c6f31d53442cdfe84d4fe08af5a9689cd2e6212a
Goat <vova.cher@yahoo.com>
2026-09-22 12:14:14 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
-'use strict';
+const menuLinks = document.querySelectorAll('.nav__link');
+
+menuLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    if (window.location.hash === '#menu') {
+      window.location.hash = link.getAttribute('href');
+    }
+  });
+});
